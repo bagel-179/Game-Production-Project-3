b0c9c58557176820202d9bb16b0eb108c6ad7aed
Matterjaws55 <163688327+Matterjaws55@users.noreply.github.com>
2025-04-06 20:30:08 -0400

    Laser WIP

---
+++ Assets/Scripts/Enemies/EnemyLaserAttack.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EnemyLaserAttack : MonoBehaviour
+{
+    [Header("Attack Settings")]
+    public GameObject attackPoint;
+    public GameObject laser;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
