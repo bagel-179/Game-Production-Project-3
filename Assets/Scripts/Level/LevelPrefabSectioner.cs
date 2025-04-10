56ae1242031eca8bb0003ca378f015d2e397bebb
Matterjaws55 <163688327+Matterjaws55@users.noreply.github.com>
2025-04-10 12:15:36 -0400

    Editing Colliders for performance

--- Assets/Scripts/Level/LevelPrefabSectioner.cs
+++ Assets/Scripts/Level/LevelPrefabSectioner.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class LevelPrefabSectioner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [System.Serializable]
+    public class ObjectToggle
     {
-        
+        public GameObject targetObject;
+        public bool setActiveOnTrigger = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    [Header("Objects to Toggle on Trigger Enter")]
+    public ObjectToggle[] objectsToToggle;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            foreach (ObjectToggle toggle in objectsToToggle)
+            {
+                if (toggle.targetObject != null)
+                {
+                    toggle.targetObject.SetActive(toggle.setActiveOnTrigger);
+                }
+            }
+        }
     }
 }
