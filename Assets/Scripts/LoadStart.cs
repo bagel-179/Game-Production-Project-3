00b986614b927a22fc62f17b268b7e1373c039ae
Matterjaws55 <163688327+Matterjaws55@users.noreply.github.com>
2025-04-27 23:14:47 -0400

    EndingFix

--- Assets/Scripts/LoadStart.cs
+++ Assets/Scripts/LoadStart.cs
@@ -4,7 +4,26 @@ using UnityEngine.SceneManagement;
 
 public class LoadStart : MonoBehaviour
 {
-    void Start()
+    public bool isBadEnding = false;
+
+    public void EndGame()
+    {
+        if (isBadEnding)
+        {
+            LoadBadEnding();
+        }
+        else
+        {
+            LoadGoodEnding();
+        }
+    }
+
+    private void LoadBadEnding()
+    {
+        SceneManager.LoadScene("ClockTowerBlockout");
+    }
+
+    private void LoadGoodEnding()
     {
         SceneManager.LoadScene("ClockTowerEnding");
     }
